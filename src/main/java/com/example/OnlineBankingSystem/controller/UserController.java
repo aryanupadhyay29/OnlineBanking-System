240b5c08a05ec78c3fa84278a6d232fe176ee04b
aryanupadhyay29 <aryanupadhyay9027@gmail.com>
2026-03-31 22:27:01 +0530

    Basic Starting api

--- src/main/java/com/example/OnlineBankingSystem/controller/UserController.java
+++ src/main/java/com/example/OnlineBankingSystem/controller/UserController.java
@@ -2,15 +2,17 @@ package com.example.OnlineBankingSystem.controller;
 
 import com.example.OnlineBankingSystem.model.User;
 import com.example.OnlineBankingSystem.service.UserService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
     private UserService userService;
+
+    @GetMapping("/")
+    public String Health(){
+        return "Server is running";
+    }
 
     @PostMapping("/register")
     public User registerUser(@RequestBody User user) {
